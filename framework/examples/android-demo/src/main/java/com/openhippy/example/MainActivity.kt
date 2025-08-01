80fecf6c0a2ef16ae96441e7394dc3e4585bfd17
liulipeng <pengliliu8086@163.com>
2025-08-01 18:29:51 +0800

    第 4 节：裁剪最小可运行 Java 工程（Android）

--- framework/examples/android-demo/src/main/java/com/openhippy/example/MainActivity.kt
+++ framework/examples/android-demo/src/main/java/com/openhippy/example/MainActivity.kt
@@ -15,69 +15,196 @@
 
 package com.openhippy.example
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.view.MotionEvent
-import android.view.View
-import android.widget.ImageView
+import android.util.Log
+import android.view.ViewGroup
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowInsetsControllerCompat
+import com.tencent.mtt.hippy.HippyAPIProvider
+import com.tencent.mtt.hippy.HippyEngine
+import com.tencent.mtt.hippy.HippyEngine.EngineInitParams
+import com.tencent.mtt.hippy.HippyEngine.EngineInitStatus
+import com.tencent.mtt.hippy.HippyEngine.EngineListener
+import com.tencent.mtt.hippy.HippyEngine.ModuleListener
+import com.tencent.mtt.hippy.HippyEngine.ModuleLoadParams
+import com.tencent.mtt.hippy.HippyEngine.ModuleLoadStatus
+import com.tencent.mtt.hippy.HippyEngine.create
+import com.tencent.mtt.hippy.adapter.DefaultLogAdapter
+import com.tencent.mtt.hippy.adapter.exception.HippyExceptionHandlerAdapter
+import com.tencent.mtt.hippy.common.HippyJsException
+import com.tencent.mtt.hippy.common.HippyMap
+import com.tencent.mtt.hippy.utils.UIThreadUtils
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var activityMainRoot: View
+  companion object {
+    private const val TAG = "MainActivity"
+    private const val DEBUG_SERVER_HOST = "192.168.40.77:38989"
+  }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        WindowInsetsControllerCompat(window, window.decorView).isAppearanceLightStatusBars = true
-        activityMainRoot = layoutInflater.inflate(R.layout.activity_main, null)
-        intPageMain()
-        setContentView(activityMainRoot)
+  private lateinit var activityMainRoot: ViewGroup
+  private var hippyEngine: HippyEngine? = null
+  private lateinit var debugButton: Button
+  private lateinit var hippyRootViewWrapper: ViewGroup
+
+  var hippyRootView: ViewGroup? = null
+  val driverMode: PageConfiguration.DriverMode = PageConfiguration.DriverMode.JS_VUE_3
+
+  private var engineId: Int? = null
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    WindowInsetsControllerCompat(window, window.decorView).isAppearanceLightStatusBars = true
+    activityMainRoot = layoutInflater.inflate(R.layout.activity_main, null) as ViewGroup
+    setContentView(activityMainRoot)
+
+    debugButton = activityMainRoot.findViewById<Button>(R.id.debug_button)
+    hippyRootViewWrapper = activityMainRoot.findViewById<ViewGroup>(R.id.hippy_root_wrapper)
+    debugButton.setText("调试：" + DEBUG_SERVER_HOST)
+    debugButton.setOnClickListener { v ->
+      initEngine()
     }
+  }
 
-    override fun onResume() {
-        super.onResume()
-        HippyEngineHelper.clearAbandonHippyEngine()
+  override fun onResume() {
+    hippyEngine?.onEngineResume()
+    super.onResume()
+  }
+
+  override fun onPause() {
+    super.onPause()
+    hippyEngine?.onEnginePause()
+  }
+
+  override fun onStop() {
+    hippyEngine?.destroyModule(hippyRootView) { result, e ->
+      hippyEngine?.destroyEngine()
     }
+    hippyRootView = null
+    super.onStop()
+  }
 
-    private fun intPageMain() {
-        val pageManagementButton = activityMainRoot.findViewById<View>(R.id.page_management_button)
-        val pageManagementImage =
-            pageManagementButton.findViewById<ImageView>(R.id.page_management_image)
-        val settingButton = activityMainRoot.findViewById<View>(R.id.setting_button)
-        val settingImage = settingButton.findViewById<ImageView>(R.id.setting_image)
-        pageManagementButton.setOnClickListener { v ->
-            val intent = Intent(this, PageManagement::class.java)
-            startActivity(intent)
-        }
-        settingButton.setOnClickListener { v ->
-            val intent = Intent(this, PageSetting::class.java)
-            startActivity(intent)
-        }
-        pageManagementButton.setOnTouchListener { v, event ->
-            when (event.action) {
-                MotionEvent.ACTION_DOWN -> {
-                    pageManagementImage.setImageResource(R.drawable.page_management_pressed_2x)
-                }
-                MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
-                    pageManagementImage.setImageResource(R.drawable.page_management_2x)
-                }
+  override fun onBackPressed() {
+    super.onBackPressed()
+    hippyEngine?.onBackPressed {}
+  }
+
+  private fun initEngine() {
+    Log.d(TAG, "initEngine started")
+    val initParams = EngineInitParams()
+    initParams.context = this@MainActivity
+    initParams.debugServerHost = DEBUG_SERVER_HOST
+    initParams.debugMode = true
+    initParams.enableLog = true
+    initParams.logAdapter = DefaultLogAdapter()
+    initParams.groupId = 1
+    when (driverMode) {
+      PageConfiguration.DriverMode.JS_REACT -> {
+        initParams.coreJSAssetsPath = "react/vendor.android.js"
+      }
+
+      PageConfiguration.DriverMode.JS_VUE_2 -> {
+        initParams.coreJSAssetsPath = "vue2/vendor.android.js"
+      }
+
+      PageConfiguration.DriverMode.JS_VUE_3 -> {
+        initParams.coreJSAssetsPath = "vue3/vendor.android.js"
+      }
+
+      PageConfiguration.DriverMode.VL -> {
+        //TODO: Coming soon
+      }
+    }
+    initParams.codeCacheTag = "common"
+    initParams.exceptionHandler = object : HippyExceptionHandlerAdapter {
+      override fun handleJsException(e: HippyJsException) {
+        Log.e(TAG, "handleJsException :" + e.message)
+      }
+
+      override fun handleNativeException(e: Exception, haveCaught: Boolean) {
+        Log.e(TAG, "handleNativeException :" + e.message)
+      }
+
+      override fun handleBackgroundTracing(details: String) {
+        Log.e(TAG, "handleBackgroundTracing :$details")
+      }
+    }
+    val providers: MutableList<HippyAPIProvider> = ArrayList()
+    providers.add(ExampleAPIProvider())
+
+    initParams.providers = providers
+    initParams.enableTurbo = true
+    hippyEngine = create(initParams)
+
+    engineId = hippyEngine?.engineId
+    Log.d(TAG, "initEngine end hippyEngine: $hippyEngine engineId: $engineId")
+    //
+    hippyEngine?.initEngine(object : EngineListener {
+      override fun onInitialized(statusCode: EngineInitStatus, msg: String?) {
+        if (statusCode == EngineInitStatus.STATUS_OK) {
+          val loadParams = ModuleLoadParams()
+          loadParams.context = this@MainActivity
+          loadParams.componentName = "Demo"
+          loadParams.codeCacheTag = "Demo"
+          when (driverMode) {
+            PageConfiguration.DriverMode.JS_REACT -> {
+              loadParams.jsAssetsPath = "react/index.android.js"
             }
-            return@setOnTouchListener false
 
-        }
-        settingButton.setOnTouchListener { v, event ->
-            when (event.action) {
-                MotionEvent.ACTION_DOWN -> {
-                    settingImage.setImageResource(R.drawable.setting_pressed_2x)
-                }
-                MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
-                    settingImage.setImageResource(R.drawable.setting_2x)
-                }
+            PageConfiguration.DriverMode.JS_VUE_2 -> {
+              loadParams.jsAssetsPath = "vue2/index.android.js"
             }
-            return@setOnTouchListener false
 
+            PageConfiguration.DriverMode.JS_VUE_3 -> {
+              loadParams.jsAssetsPath = "vue3/index.android.js"
+            }
+
+            PageConfiguration.DriverMode.VL -> {
+              //TODO: Coming soon
+            }
+          }
+          loadParams.jsFilePath = null
+          loadParams.jsParams = HippyMap()
+          loadParams.jsParams.pushString(
+            "msgFromNative", "Hi js developer, I come from native code!"
+          )
+          hippyRootView = hippyEngine?.loadModule(loadParams, object : ModuleListener {
+            override fun onLoadCompleted(statusCode: ModuleLoadStatus, msg: String?) {
+              Log.e(TAG, "onLoadCompleted: statusCode: $statusCode msg:$msg")
+            }
+
+            override fun onJsException(exception: HippyJsException): Boolean {
+              Log.e(TAG, "onJsException:" + exception.message)
+              return true
+            }
+
+            override fun onFirstViewAdded() {
+              Log.e(TAG, "onFirstViewAdded")
+            }
+
+            override fun onFirstContentfulPaint() {
+              Log.e(TAG, "onFirstContentfulPaint")
+            }
+          })
+
+          val loadCallbackTask = Runnable {
+            hippyRootViewWrapper.addView(
+              hippyRootView,
+              ViewGroup.LayoutParams.MATCH_PARENT,
+              ViewGroup.LayoutParams.MATCH_PARENT
+            )
+          }
+          if (UIThreadUtils.isOnUiThread()) {
+            loadCallbackTask.run()
+          } else {
+            UIThreadUtils.runOnUiThread {
+              loadCallbackTask.run()
+            }
+          }
         }
-    }
+      }
+    })
+  }
 }
